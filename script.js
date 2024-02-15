03b9e6d1c1d740bb7d4107c95bf6c471fd25c431
Alexandr <61061837+AlexandrVanchukov@users.noreply.github.com>
2024-02-15 16:00:43 +0300

    Create blog section

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
-document.querySelectorAll('.dropdown').forEach(function(dropDownWrapper){
-    
-    const dropDownButton = dropDownWrapper.querySelector(".dropdown__button");
+document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
+    const dropDownButton = dropDownWrapper.querySelector('.dropdown__button');
     const dropDownList = dropDownWrapper.querySelector('.dropdown__list');
     const dropDownItems = dropDownList.querySelectorAll('.dropdown__list-item');
-    const dropDownInput = dropDownWrapper.querySelector('.dropdown__input-hidden');
+    const dropDownInput = dropDownWrapper.querySelector(
+        '.dropdown__input-hidden'
+    );
 
     //Клик по кнопке открыть/закрыть select
-    dropDownButton.addEventListener('click', function (){
+    dropDownButton.addEventListener('click', function () {
         dropDownList.classList.toggle('dropdown__list--visible');
         this.classList.add('dropdown__button--active');
-        
-    })
+    });
 
     //Выбор элемента из списка. Запонить выбранное значение. Закрыть дропдаун.
-    dropDownItems.forEach(function(listItem){
-        listItem.addEventListener('click', function(e){
+    dropDownItems.forEach(function (listItem) {
+        listItem.addEventListener('click', function (e) {
             e.stopPropagation();
             dropDownButton.innerText = this.innerText;
             dropDownButton.focus();
             dropDownInput.value = this.dataset.value;
             dropDownList.classList.remove('dropdown__list--visible');
-
-        })
-    })
+        });
+    });
 
     //клик снаружи дроп дауна. Закрыть дропдаун
     document.addEventListener('click', function (e) {
-        if(e.target !== dropDownButton){
+        if (e.target !== dropDownButton) {
             dropDownList.classList.remove('dropdown__list--visible');
             dropDownButton.classList.remove('dropdown__button--active');
         }
-    })
+    });
 
     //Закртыие на Esc и Tab
     document.addEventListener('keydown', function (e) {
-        if ( e.key === 'Tab' || e.key === 'Escape') {
+        if (e.key === 'Tab' || e.key === 'Escape') {
             dropDownList.classList.remove('dropdown__list--visible');
             dropDownButton.classList.remove('dropdown__button--active');
         }
-    })
-})
+    });
+});
 
+function test(a, b) {
+    const sum = 0;
+    const meme = k + b;
+    return 'ddad';
+}
